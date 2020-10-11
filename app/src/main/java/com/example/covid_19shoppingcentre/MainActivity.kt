6d372d669bcf12a9f3361741786764efef10a3d3
yiyang1016 <yiyang908@gmail.com>
2020-10-11 16:40:14 +0800

    new file

--- app/src/main/java/com/example/covid_19shoppingcentre/MainActivity.kt
+++ app/src/main/java/com/example/covid_19shoppingcentre/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.covid_19shoppingcentre
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    button.setOnClickListener{
+        val intent = Intent(this, Store_List::class.java)
+        startActivity(intent)
+    }
     }
 }
