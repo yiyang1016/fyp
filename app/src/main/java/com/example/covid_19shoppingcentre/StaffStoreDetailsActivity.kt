67674efde479bb6ba45ec93002d147f528fcaa82
jasonQuah <jasonquah3343@gmail.com>
2020-11-15 01:05:22 +0800

    SOS

--- app/src/main/java/com/example/covid_19shoppingcentre/StaffStoreDetailsActivity.kt
+++ app/src/main/java/com/example/covid_19shoppingcentre/StaffStoreDetailsActivity.kt
@@ -1,6 +1,5 @@
 package com.example.covid_19shoppingcentre
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -11,11 +10,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.google.firebase.database.*
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.customer_in_store_layout.view.*
-import kotlinx.android.synthetic.main.list_layout.view.*
-import kotlinx.android.synthetic.main.reserve_store_details.*
 import kotlinx.android.synthetic.main.staff_store_details.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -28,7 +30,7 @@ class StaffStoreDetailsActivity : AppCompatActivity() {
     lateinit var FirebaseRecyclerAdapter : FirebaseRecyclerAdapter<CheckInScCustomer, CustomerViewHolder>
 
     // CONSTANT
-    private val EVERY_ONE_SECOND: Long = 1000
+    private val EVERY_ONE_SECOND: Long = 10000
 
     // Run on parallel
     private var handler: Handler? = null
@@ -47,6 +49,68 @@ class StaffStoreDetailsActivity : AppCompatActivity() {
         val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
         val dateText = currentDateTime.format(dateFormat)
 
+        val linechart = findViewById<LineChart>(R.id.storeLinechart)
+
+        val entries = ArrayList<Entry>()
+
+        for (x in 8..22) {
+            val data1 = Database.child("CheckInStore").child(dateText.toString()).child(storeId).child(x.toString())
+
+            data1.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(q0: DataSnapshot) {
+
+                    if (q0.exists()) {
+                        var floaat1 = q0.childrenCount.toFloat()
+                        var floaat = x.toFloat()
+                        entries.add(Entry(floaat, floaat1))
+                    } else {
+                        var floaat = x.toFloat()
+                        entries.add(Entry(floaat, 0f))
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(applicationContext, "ERROR", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        entries.add(Entry(7f, 0f))
+
+        val v1 = LineDataSet(entries, "Number of Customer")
+
+        v1.setDrawValues(true)
+        v1.setDrawFilled(true)
+        v1.lineWidth = 2f
+        v1.fillColor = R.color.white
+        v1.fillAlpha = R.color.white
+
+        linechart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        linechart.axisLeft.setDrawGridLines(false);
+        linechart.xAxis.setDrawGridLines(false);
+
+        linechart.data = LineData(v1)
+
+        linechart.xAxis.labelRotationAngle = 0f
+
+        linechart.axisRight.isEnabled = false
+        linechart.xAxis.axisMaximum = 22f
+        linechart.xAxis.axisMinimum = 8f
+
+        linechart.axisLeft.axisMinimum = 0f
+        linechart.axisLeft.axisMaximum = 30f
+
+        linechart.setTouchEnabled(true)
+        linechart.setPinchZoom(true)
+
+        linechart.description.text = "Hours"
+        linechart.setNoDataText("No customer yet!")
+
+        linechart.animateX(1800, Easing.EaseInExpo)
+
+
+
         mRecyclerView = findViewById(R.id.customerInStoreList)
         mRecyclerView.setHasFixedSize(true)
         mRecyclerView.setLayoutManager(LinearLayoutManager(this))
@@ -58,6 +122,74 @@ class StaffStoreDetailsActivity : AppCompatActivity() {
         customerNumber()
         logRecyclerView()
         executeHandler()
+    }
+
+    private fun chart(){
+        val currentDateTime = LocalDateTime.now()
+        val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val dateText = currentDateTime.format(dateFormat)
+        val storeId = intent.getStringExtra("StoreId")
+
+        val linechart = findViewById<LineChart>(R.id.storeLinechart)
+
+        val entries1 = ArrayList<Entry>()
+
+        for (x in 8..22) {
+            val data1 = Database.child("CheckInStore").child("20201114").child(storeId).child(x.toString())
+
+            data1.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(q0: DataSnapshot) {
+
+                    if (q0.exists()) {
+                        var floaat1 = q0.childrenCount.toFloat()
+                        var floaat = x.toFloat()
+                        entries1.add(Entry(floaat, floaat1))
+                        Toast.makeText(applicationContext, floaat1.toString() + "ERROR", Toast.LENGTH_SHORT).show()
+                    } else {
+                        var floaat = x.toFloat()
+                        entries1.add(Entry(floaat, 0f))
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(applicationContext, "ERROR", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        entries1.add(Entry(7f, 0f))
+
+        val v2 = LineDataSet(entries1, "Number of Customer")
+
+        v2.setDrawValues(true)
+        v2.setDrawFilled(true)
+        v2.lineWidth = 2f
+        v2.fillColor = R.color.white
+        v2.fillAlpha = R.color.white
+
+        linechart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        linechart.axisLeft.setDrawGridLines(false);
+        linechart.xAxis.setDrawGridLines(false);
+
+        linechart.data = LineData(v2)
+
+        linechart.xAxis.labelRotationAngle = 0f
+
+        linechart.axisRight.isEnabled = false
+        linechart.xAxis.axisMaximum = 22f
+        linechart.xAxis.axisMinimum = 8f
+
+        linechart.axisLeft.axisMinimum = 0f
+        linechart.axisLeft.axisMaximum = 30f
+
+        linechart.setTouchEnabled(true)
+        linechart.setPinchZoom(true)
+
+        linechart.description.text = "Hours"
+        linechart.setNoDataText("No customer yet!")
+
+        //linechart.animateY(1000, Easing.EaseInExpo)
     }
 
     private fun logRecyclerView(){
@@ -114,6 +246,7 @@ class StaffStoreDetailsActivity : AppCompatActivity() {
             override fun onDataChange(p0: DataSnapshot) {
                 for (p0 in p0.children) {
                     if(p0.child("status").value.toString() == "active") {
+
                         customerCountInt++
                     }
                 }
@@ -137,6 +270,7 @@ class StaffStoreDetailsActivity : AppCompatActivity() {
                 @RequiresApi(Build.VERSION_CODES.O)
                 override fun run() {
                     //Updating firebase store/getting
+                    chart()
                     customerNumber()
                     //And we execute it again
                     handler!!.postDelayed(this, EVERY_ONE_SECOND)
